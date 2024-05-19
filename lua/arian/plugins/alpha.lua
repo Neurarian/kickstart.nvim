b4d971bfc8f40896f64ddd88408233eb1b208be8
Neurarian <110474238+Neurarian@users.noreply.github.com>
2024-05-19 22:28:31 +0200

    alpha custom theme

--- lua/arian/plugins/alpha.lua
+++ lua/arian/plugins/alpha.lua
@@ -7,15 +7,11 @@ return {
   },
   config = function()
     local theta = require 'alpha.themes.theta'
+    local dashboard = require 'alpha.themes.dashboard'
+    vim.api.nvim_set_hl(0, 'Type', { fg = '#89B4FA' })
+    vim.api.nvim_set_hl(0, 'SpecialComment', { fg = '#A6E3A1' })
 
     theta.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
       [[                                                                     ]],
       [[       ████ ██████           █████      ██                     ]],
       [[      ███████████             █████                             ]],
@@ -27,6 +23,30 @@ return {
       [[                                                                       ]],
       [[                                                                       ]],
       [[                                                                       ]],
+    }
+
+    theta.header.opts.hl = {
+      { { 'Type', 0, 195 } },
+      { { 'SpecialComment', 0, 61 }, { 'Type', 61, 195 } },
+      { { 'SpecialComment', 0, 64 }, { 'Type', 64, 195 } },
+      { { 'SpecialComment', 0, 91 }, { 'Type', 91, 195 } },
+      { { 'SpecialComment', 0, 86 }, { 'Type', 86, 195 } },
+      { { 'SpecialComment', 0, 99 }, { 'Type', 99, 195 } },
+      { { 'SpecialComment', 0, 98 }, { 'Type', 98, 195 } },
+      { { 'SpecialComment', 0, 109 }, { 'Type', 109, 195 } },
+      { { 'SpecialComment', 0, 195 } },
+      { { 'SpecialComment', 0, 195 } },
+      { { 'SpecialComment', 0, 195 } },
+    }
+    theta.buttons.val = {
+      { type = 'text', val = 'Quick links', opts = { hl = 'Type', position = 'center' } },
+      { type = 'padding', val = 1 },
+      dashboard.button('e', '  New file', '<cmd>ene<CR>'),
+      dashboard.button('SPC f f', '󰈞  Find file'),
+      dashboard.button('SPC f g', '󰊄  Live grep'),
+      dashboard.button('c', '  Configuration', '<cmd>cd ~/.config/nvim/ <CR>'),
+      dashboard.button('u', '  Update plugins', '<cmd>Lazy sync<CR>'),
+      dashboard.button('q', '󰅚  Quit', '<cmd>qa<CR>'),
     }
 
     require('alpha').setup(theta.config)
